808c9448515c4ea98925a781bfb86d49693b979e
XoriieInpottn <XoriieInpottn@126.com>
2024-12-31 18:50:54 +0800

    support hybrid search

--- libdata/milvus.py
+++ libdata/milvus.py
@@ -5,14 +5,16 @@ __all__ = [
     "LazyMilvusClient",
 ]
 
-from typing import Any, Dict, List, Optional, Union
+from datetime import datetime
+from typing import Any, Dict, List, Mapping, Optional, Union
 
 import numpy as np
+from scipy.sparse import csr_array
 
 from libdata.common import ConnectionPool, LazyClient, ParsedURL
 
 DEFAULT_CONN_POOL_SIZE = 16
-DEFAULT_VARCHAR_LENGTH = 65536
+DEFAULT_VARCHAR_LENGTH = 65535
 DEFAULT_ID_LENGTH = 256
 DEFAULT_INDEX_CONFIG = {
     "AUTOINDEX": {
@@ -26,6 +28,9 @@ DEFAULT_INDEX_CONFIG = {
         "params": {"M": 8, "efConstruction": 64}
     }
 }
+
+DenseVector = Union[np.ndarray, list]
+SparseVector = Union[csr_array, dict]
 
 
 class LazyMilvusClient(LazyClient):
@@ -96,17 +101,20 @@ class LazyMilvusClient(LazyClient):
     def exists(self, timeout: Optional[float] = None) -> bool:
         return self.client.has_collection(self.collection_name, timeout=timeout)
 
+    def flush(self, timeout: Optional[float] = None):
+        self.client.flush(self.collection_name, timeout=timeout)
+
     # noinspection PyPackageRequirements
     def create(
             self,
             ref_doc: Dict[str, Any],
             id_field: str = "id",
             dynamic_field: bool = True,
-            dense_index: Union[str, Dict] = "AUTOINDEX",
+            vector_index: Union[str, Dict] = "AUTOINDEX",
+            sparse_vector_index: Union[str, Dict] = "AUTOINDEX",
             timeout: Optional[float] = None
     ):
         from pymilvus import DataType
-        from pymilvus.orm.types import infer_dtype_bydata
 
         if id_field not in ref_doc:
             # ID field not given in the ref_doc means the collection need to use auto_id mode.
@@ -115,13 +123,15 @@ class LazyMilvusClient(LazyClient):
         else:
             schema = self.client.create_schema(auto_id=False, enable_dynamic_field=dynamic_field)
             id_value = ref_doc[id_field]
-            id_dtype = infer_dtype_bydata(id_value)
-            if id_dtype is DataType.INT64:
-                schema.add_field(field_name=id_field, datatype=DataType.INT64, is_primary=True)
-            elif id_dtype is DataType.VARCHAR:
-                schema.add_field(field_name=id_field, datatype=DataType.VARCHAR, is_primary=True, max_length=128)
+            field_kwargs = {"is_primary": True}
+            if isinstance(id_value, int):
+                field_kwargs["datatype"] = DataType.INT64
+            elif isinstance(id_value, str):
+                field_kwargs["datatype"] = DataType.VARCHAR
+                field_kwargs["max_length"] = DEFAULT_ID_LENGTH
             else:
-                raise TypeError(f"Invalid id dtype \"{id_dtype}\". Should be one of INT64 or VARCHAR().")
+                raise TypeError(f"Invalid id type \"{type(id_value)}\". Should be one of int or str.")
+            schema.add_field(field_name=id_field, **field_kwargs)
 
         index_params = self.client.prepare_index_params()
         index_params.add_index(id_field)
@@ -130,25 +140,27 @@ class LazyMilvusClient(LazyClient):
             if field == id_field:
                 # ID field is already added.
                 continue
-            dtype = infer_dtype_bydata(value)
-            kwargs = {}
-            if dtype is DataType.UNKNOWN:
-                raise TypeError(f"Unsupported data type {type(value)}.")
-            elif dtype is DataType.VARCHAR:
-                kwargs["max_length"] = 65535
-            elif dtype in {DataType.FLOAT_VECTOR, DataType.FLOAT16_VECTOR, DataType.BFLOAT16_VECTOR}:
-                kwargs["dim"] = len(value)
-                if isinstance(dense_index, str):
-                    dense_index = DEFAULT_INDEX_CONFIG[dense_index]
+
+            field_kwargs = self._infer_dtype(value)
+            if field_kwargs["datatype"] is DataType.FLOAT_VECTOR:
+                if isinstance(vector_index, str):
+                    vector_index = DEFAULT_INDEX_CONFIG[vector_index]
                 index_params.add_index(
                     field_name=field,
-                    index_type=dense_index["index_type"],
-                    metric_type=dense_index["metric_type"],
-                    **dense_index["params"]
+                    index_type=vector_index["index_type"],
+                    metric_type=vector_index["metric_type"],
+                    **vector_index["params"]
                 )
-            elif dtype is DataType.SPARSE_FLOAT_VECTOR:
-                raise NotImplementedError()
-            schema.add_field(field_name=field, datatype=dtype, **kwargs)
+            elif field_kwargs["datatype"] is DataType.SPARSE_FLOAT_VECTOR:
+                if isinstance(sparse_vector_index, str):
+                    sparse_vector_index = DEFAULT_INDEX_CONFIG[sparse_vector_index]
+                index_params.add_index(
+                    field_name=field,
+                    index_type=sparse_vector_index["index_type"],
+                    metric_type=sparse_vector_index["metric_type"],
+                    **sparse_vector_index["params"]
+                )
+            schema.add_field(field_name=field, **field_kwargs)
 
         self.client.create_collection(
             collection_name=self.collection_name,
@@ -157,66 +169,175 @@ class LazyMilvusClient(LazyClient):
             timeout=timeout
         )
 
+    # noinspection PyPackageRequirements
+    @staticmethod
+    def _infer_dtype(value):
+        from pymilvus import DataType
+
+        if isinstance(value, str):
+            return {"datatype": DataType.VARCHAR, "max_length": DEFAULT_VARCHAR_LENGTH}
+        elif isinstance(value, int):
+            return {"datatype": DataType.INT64}
+        elif isinstance(value, float):
+            return {"datatype": DataType.FLOAT}
+        elif isinstance(value, np.ndarray):
+            shape = value.shape
+            if len(shape) == 1 or (len(shape) == 2 and shape[0] == 1):
+                return {"datatype": DataType.FLOAT_VECTOR, "dim": shape[-1]}
+            raise ValueError(f"Invalid vector shape {shape}.")
+        elif isinstance(value, csr_array):
+            return {"datatype": DataType.SPARSE_FLOAT_VECTOR}
+        elif isinstance(value, datetime):
+            return {"datatype": DataType.VARCHAR, "max_length": 24}
+        elif isinstance(value, List):
+            if len(value) > 0:
+                if all(isinstance(v, (float, int)) for v in value):
+                    return {"datatype": DataType.FLOAT_VECTOR, "dim": len(value)}
+                elif all(
+                        isinstance(v, (tuple, list))
+                        and len(v) == 2
+                        and isinstance(v[0], int)
+                        and isinstance(v[1], (float, int))
+                        for v in value
+                ):
+                    return {"datatype": DataType.SPARSE_FLOAT_VECTOR}
+            return {"datatype": DataType.JSON}
+        elif isinstance(value, Mapping):
+            if len(value) > 0:
+                if all(isinstance(k, int) and isinstance(v, (float, int)) for k, v in value.items()):
+                    return {"datatype": DataType.SPARSE_FLOAT_VECTOR}
+            return {"datatype": DataType.JSON}
+        else:
+            raise TypeError(f"Unsupported data type {type(value)}.")
+
+    @staticmethod
+    def _prepare_doc_for_insert(doc: dict):
+        converted_doc = {}
+        for name, value in doc.items():
+            if isinstance(value, datetime):
+                value = value.strftime("%Y-%m-%d %H:%M:%S")
+            elif isinstance(value, np.ndarray):
+                shape = value.shape
+                if len(shape) == 1:
+                    value = value.tolist()
+                elif len(shape) == 2 and shape[0] == 1:
+                    value = value[0].tolist()
+                else:
+                    raise ValueError("Only one vector can be used.")
+            elif isinstance(value, csr_array):
+                shape = value.shape
+                if len(shape) == 1:
+                    value = [{i: v for i, v in zip(value.indices, value.data)}]
+                elif len(shape) == 2 and shape[0] == 1:
+                    start, end = value.indptr[0], value.indptr[1]
+                    value = {i: v for i, v in zip(value.indices[start:end], value.data[start:end])}
+                else:
+                    raise ValueError("Only one vector can be used.")
+            converted_doc[name] = value
+        return converted_doc
+
     def drop(self, timeout: Optional[float] = None):
         self.client.drop_collection(self.collection_name, timeout=timeout)
 
     def insert(self, docs: Union[dict, List[dict]], timeout: Optional[float] = None) -> dict:
-        if not self.exists(timeout=timeout):
-            if isinstance(docs, List):
-                if len(docs) == 0:
-                    return {"insert_count": 0, "ids": []}
-                ref_doc = docs[0]
-            else:
-                ref_doc = docs
-            self.create(ref_doc)
-        return self.client.insert(self.collection_name, docs)
+        if not isinstance(docs, List):
+            docs = [docs]
 
-    def upsert(self, docs: Union[Dict, List[Dict]], timeout: Optional[float] = None) -> dict:
+        if len(docs) == 0:
+            return {"insert_count": 0, "ids": []}
+
         if not self.exists(timeout=timeout):
-            if isinstance(docs, List):
-                if len(docs) == 0:
-                    return {"upsert_count": 0, "ids": []}
-                ref_doc = docs[0]
-            else:
-                ref_doc = docs
-            self.create(ref_doc)
-        return self.client.upsert(
-            collection_name=self.collection_name,
+            self.create(docs[0])
+
+        docs = [self._prepare_doc_for_insert(doc) for doc in docs]
+
+        return self.client.insert(
+            self.collection_name,
             data=docs,
             timeout=timeout
         )
 
-    # noinspection PyShadowingBuiltins
+    def upsert(self, docs: Union[Dict, List[Dict]], timeout: Optional[float] = None) -> dict:
+        if not isinstance(docs, List):
+            docs = [docs]
+
+        if len(docs) == 0:
+            return {"insert_count": 0, "ids": []}
+
+        if not self.exists(timeout=timeout):
+            self.create(docs[0])
+
+        docs = [self._prepare_doc_for_insert(doc) for doc in docs]
+
+        return self.client.upsert(
+            self.collection_name,
+            data=docs,
+            timeout=timeout
+        )
+
     def delete(
             self,
             ids: Optional[Union[list, str, int]] = None,
-            filter: Optional[str] = None,
+            expr: Optional[str] = None,
             timeout: Optional[float] = None,
     ) -> dict:
-        return self.client.delete(self.collection_name, ids=ids, filter=filter, timeout=timeout)
+        return self.client.delete(self.collection_name, ids=ids, filter=expr, timeout=timeout)
 
-    # noinspection PyShadowingBuiltins
     def query(
             self,
-            filter: str = "",
+            expr: str = "",
             ids: Optional[Union[List, str, int]] = None,
             output_fields: Optional[List[str]] = None,
             timeout: Optional[float] = None,
     ) -> List[dict]:
         return self.client.query(
             self.collection_name,
-            filter=filter,
+            filter=expr,
             output_fields=output_fields,
             timeout=timeout,
             ids=ids,
         )
 
-    # noinspection PyShadowingBuiltins
+    @staticmethod
+    def _prepare_vector_for_search(vector):
+        if isinstance(vector, np.ndarray):
+            shape = vector.shape
+            if len(shape) == 1:
+                return [vector.tolist()]
+            elif len(shape) == 2:
+                return vector.tolist()
+            else:
+                raise ValueError("Vector shape cannot be larger than 2.")
+        elif isinstance(vector, csr_array):
+            shape = vector.shape
+            if len(shape) == 1:
+                return [{i: v for i, v in zip(vector.indices, vector.data)}]
+            elif len(shape) == 2:
+                return [
+                    {i: v for i, v in zip(
+                        vector.indices[vector.indptr[row]:vector.indptr[row + 1]],
+                        vector.data[vector.indptr[row]:vector.indptr[row + 1]]
+                    )}
+                    for row in range(shape[0])
+                ]
+            else:
+                raise ValueError("Vector shape cannot be larger than 2.")
+        elif isinstance(vector, dict):
+            return [vector]
+        elif isinstance(vector, list):
+            if len(vector) == 0:
+                raise ValueError("Vector cannot be an empty list.")
+            if not isinstance(vector[0], List):
+                return [vector]
+            return vector
+        else:
+            raise ValueError(f"Unsupported vector type {type(vector)}.")
+
     def search(
             self,
             field: str,
-            data: Union[List[list], list, np.ndarray],
-            filter: str = "",
+            data: Union[DenseVector, SparseVector, List[DenseVector], List[SparseVector]],
+            expr: str = "",
             limit: int = 10,
             output_fields: Optional[List[str]] = None,
             search_params: Optional[dict] = None,
@@ -225,23 +346,52 @@ class LazyMilvusClient(LazyClient):
         if not self.client.has_collection(self.collection_name):
             return []
 
-        if isinstance(data, np.ndarray):
-            if len(data.shape) == 1:
-                data = [data.tolist()]
-            else:
-                data = data.tolist()
-        elif isinstance(data, list):
-            if not isinstance(data[0], list):
-                data = [data]
-
+        data = self._prepare_vector_for_search(data)
         response = self.client.search(
             self.collection_name,
             data,
-            filter=filter,
+            filter=expr,
             limit=limit,
             output_fields=output_fields,
             anns_field=field,
             search_params=search_params,
+            timeout=timeout,
+        )
+        return response[0]
+
+    # noinspection PyPackageRequirements
+    def hybrid_search(
+            self,
+            data: Mapping[str, Union[DenseVector, SparseVector, List[DenseVector], List[SparseVector]]],
+            expr: str = "",
+            limit: int = 10,
+            output_fields: Optional[List[str]] = None,
+            search_params: Optional[dict] = None,
+            timeout: Optional[float] = None,
+    ):
+        if not self.client.has_collection(self.collection_name):
+            return []
+
+        from pymilvus import AnnSearchRequest, RRFRanker
+
+        reqs = []
+        for ann_field, vector in data.items():
+            vector = self._prepare_vector_for_search(vector)
+            reqs.append(AnnSearchRequest(
+                vector,
+                ann_field,
+                param=search_params if search_params else {},
+                limit=limit,
+                expr=expr
+            ))
+
+        response = self.client.hybrid_search(
+            self.collection_name,
+            reqs,
+            RRFRanker(k=60),
+            filter=expr,
+            limit=limit,
+            output_fields=output_fields,
             timeout=timeout,
         )
         return response[0]
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         packages=[
             "libdata",
         ],
-        version="0.7",
+        version="0.8",
         description="Unified interface to access data.",
         long_description_content_type="text/markdown",
         long_description=long_description,
@@ -30,6 +30,7 @@ if __name__ == "__main__":
             "pydantic",
             "tqdm",
             "PyYAML",
-            "numpy"
+            "numpy",
+            "scipy"
         ]
     )
